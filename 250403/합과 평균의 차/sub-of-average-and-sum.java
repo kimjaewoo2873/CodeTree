61a0d0c484092c019e27b822782eb9a576be3b0d
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-04-03 17:24:07 +0900

    Title: 합과 평균의 차, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250403/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        int c = 5;
+        int sum = a + b + c;
+        int avg = sum / 3;
+        System.out.println(sum);
+        System.out.println(avg);
+        System.out.println(sum - avg);
+    }
+}
