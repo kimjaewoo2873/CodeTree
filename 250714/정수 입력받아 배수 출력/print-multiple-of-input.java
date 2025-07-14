d4f380deaf09411cc46a76d36ef4575fefe15dbb
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-14 21:20:32 +0900

    Title: 정수 입력받아 배수 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250714/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int n = scanner.nextInt();
+        for(int i=1;i<=5;i++) { 
+            System.out.print((n*i) + " ");
+        }
+    }
+}
