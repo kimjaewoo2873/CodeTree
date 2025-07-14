3a158f4b82e02a8398dea2cc40c03e3068ac9c85
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-14 20:44:41 +0900

    Title: 문자 출력하기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250714/문자 출력하기/print-text.java
+++ 250714/문자 출력하기/print-text.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner scanner = new Scanner(System.in);
-        String c = scanner.next();
+        char c = scanner.next().charAt(0);
         for(int i=0; i<8; i++) {
             System.out.println(c);
         }
