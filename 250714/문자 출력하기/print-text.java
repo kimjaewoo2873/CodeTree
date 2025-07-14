de1319c6abc3cc679a62c9b1725ce6665ed717f8
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-14 20:42:07 +0900

    Title: 문자 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250714/문자 출력하기/print-text.java
+++ 250714/문자 출력하기/print-text.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner scanner = new Scanner(System.in);
-        char c = scanner.next();
+        char c = scanner.next().charAt(0);
         for(int i=0; i<8; i++) {
             System.out.println(c);
         }
