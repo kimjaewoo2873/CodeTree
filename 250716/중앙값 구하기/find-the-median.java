debd59d26ce6dd1db25a61e7a5124f28346ab86b
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-16 00:03:11 +0900

    Title: 중앙값 구하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250716/중앙값 구하기/find-the-median.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+        
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        if((A < -100 && A > 100) || (B < -100 && B > 100) || (C < -100 && C > 100) || 
+        (A == B) || (B == C) || (A == C)) { 
+            return;
+        } 
+        else {
+            if(A <= B && A >= C) {
+                System.out.println(A);
+            }
+            else if(A <= C && A >= B) {
+                System.out.println(A);
+            } 
+            else if(B <= A && B >= C) { 
+                System.out.println(B);
+            }
+            else if(B <= C && B >= A) {
+                System.out.println(A);
+            }
+            else if(C <= A && C >= B) {
+                System.out.println(C);
+            }
+            else if(C <= B && C >= A) {
+                System.out.println(C);
+            }
+        }
+    }
+}
