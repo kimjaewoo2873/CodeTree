d2f130cbcab9eb6c2aca2860ce8a09eeb4895747
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-17 23:28:34 +0900

    Title: 친근하지 않은 수, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250717/친근하지 않은 수/unfriendly-number.java
+++ 250717/친근하지 않은 수/unfriendly-number.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        if(N < 0 || N > 100) 
-            return;
-        int much=0;
-        for(int i=1;i<=N;i++) {
-            if(i % 2 ==0 || i % 3 == 0 || i % 5 == 0) {
+        
+        // 변수 선언
+        int n;
+        int cnt = 0;
+
+        n = sc.nextInt();
+        
+        // 1부터 n까지의 수 중, 2 또는 3 또는 5로 나누어지지 않는 수의 개수를 구합니다.
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0)
                 continue;
-            } else {
-                much++;
-            }
+            cnt++;
         }
-        System.out.println(much);
+
+        // 출력
+        System.out.print(cnt);
     }
 }
