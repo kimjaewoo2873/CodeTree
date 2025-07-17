cbd7a3556643c3b2453f55fae2adcb2eaa67f672
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-17 23:23:44 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

--- 250717/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
+++ 250717/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -2,28 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        if(N < 0 || N > 100)
-            return;
-        int arr[] = new int[N];
-        int rarr[] = new int[N];
-        for(int i=0;i<arr.length;i++) {
+
+        // 정수의 개수 n을 입력받습니다.
+        int n = sc.nextInt();
+
+        // 변수를 선언합니다.
+        int[] arr = new int[100];
+
+        // 배열에 정수들을 차례로 입력받습니다.
+        for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for(int i=0;i<arr.length - 1;i++) {
-            if(arr[i] == arr[i + 1])
-                return;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            rarr[i] = arr[arr.length - 1 - i];
-        }
 
-        for(int i=0;i<rarr.length;i++) {
-            if(rarr[i] % 2 == 0) {
-                System.out.print(rarr[i] + " ");
-            }
+        // 입력받은 수들을 거꾸로 보면서 짝수인 값만 출력합니다.
+        for(int i = n - 1; i >= 0; i--) {
+            if(arr[i] % 2 == 0)
+                System.out.print(arr[i] + " ");
         }
     }
 }
