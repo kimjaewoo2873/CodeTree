eb7710c8999f22801498f37ded1a365ea96a6aa6
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-17 23:31:59 +0900

    Title: 1부터의 합, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250717/1부터의 합/sum-from-the-one.java
+++ 250717/1부터의 합/sum-from-the-one.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        if(N < 1 || N > 5000)
-            return;
+        
+        // 변수 선언
+        int n;
         int sum = 0;
-        for(int i=1;i<=100;i++) { 
-            sum+=i;
-            if(sum > N) {
-                System.out.println(i);
+
+        n = sc.nextInt();
+        
+        // 1부터 증가시키며 더한 값이 n이상이 된 순간, 더해진 숫자를 출력합니다.
+        for(int i = 1; i <= 100; i++) {
+            sum += i;
+            if(sum >= n) {
+                System.out.print(i);
                 break;
             }
         }
