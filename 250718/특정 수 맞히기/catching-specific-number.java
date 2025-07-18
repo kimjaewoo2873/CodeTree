106588976d13330f87a66b25de5b20c039ec48e1
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-18 22:50:47 +0900

    Title: 특정 수 맞히기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250718/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int goal = 25;
+        int input = 0;
+        while(true) { 
+            input = sc.nextInt();
+            if(input > goal) {
+                System.out.println("Higher");    
+            } else if (input < goal) {
+                System.out.println("Lower");
+            } else {
+                System.out.println("Good");
+                break;
+            }
+        }
+    }
+}
