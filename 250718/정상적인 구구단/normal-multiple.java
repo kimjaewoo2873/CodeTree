42db09d754266a8eb0880643ccc5ca52fe9ee143
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-18 23:31:59 +0900

    Title: 정상적인 구구단, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250718/정상적인 구구단/normal-multiple.java
+++ 250718/정상적인 구구단/normal-multiple.java
@@ -6,7 +6,7 @@ public class Main {
         int N = sc.nextInt();
         for(int i=1;i<=N;i++) { 
             for(int j=1;j<=N;j++) {
-                System.out.print(i + " * " + j + " = " + (i*j));
+                System.out.print(i + " * " + j + " = " + (i*j)+ ", ");
             }
             System.out.println();
         }
