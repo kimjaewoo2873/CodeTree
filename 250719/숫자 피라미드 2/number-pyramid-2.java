0ad716fb569593bee89094cce98acb3449fcdf3d
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-19 21:40:11 +0900

    Title: 숫자 피라미드 2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250719/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 0;
+        int floor = 0;
+        for(int i=1;i<=N;i++) { 
+            for(int j=0;;j++) {
+                cnt++;
+                System.out.print(cnt + " ");
+                floor++;
+                if(floor == i)
+                    break;
+            }
+            System.out.println();
+        }
+    }
+}
