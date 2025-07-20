7ec04be5f4a8afa04fc0371bcfa9de29fe31e85a
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-20 23:45:06 +0900

    Title: 좌우로 반복하며 출력, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250720/좌우로 반복하며 출력/print-in-left-right-reverse.java
+++ 250720/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -1,29 +1,25 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();  // 최대 4 이하로 입력해야 안전
+        int[][] arr = new int[4][4];
 
-        int arr[][] = new int[4][4];
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = j + 1;
+        for (int i = 0; i < 4; i++) {
+            if (i % 2 == 0) { // 짝수 행: 1 2 3 4
+                for (int j = 0; j < 4; j++) {
+                    arr[i][j] = j + 1;
+                }
+            } else { // 홀수 행: 4 3 2 1
+                for (int j = 0; j < 4; j++) {
+                    arr[i][j] = 4 - j;
+                }
             }
         }
 
-        for (int i = 2; i < 4; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = n - j;
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+        // 출력
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j]);
             }
             System.out.println();
         }
     }
 }
-
