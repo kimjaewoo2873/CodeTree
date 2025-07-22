3c995c995e6a15b9a6b6df21ad0d1ed10e9df9c1
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-22 23:36:43 +0900

    Title: 합을 복사하기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250722/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        a = a + b + c;
+        b = a + b + c;
+        c = a + b + c;
+    }
+}
