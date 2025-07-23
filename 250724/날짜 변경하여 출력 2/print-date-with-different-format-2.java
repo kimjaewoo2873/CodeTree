c722ab1e54a4aab110b7033d9f41f27d09b201eb
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-24 00:15:24 +0900

    Title: 날짜 변경하여 출력 2, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

--- 250724/날짜 변경하여 출력 2/print-date-with-different-format-2.java
+++ 250724/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         String parts[] = str.split("-");
-        int mm = Integer.parseInt(sb[0]);
-        int dd = Integer.parseInt(sb[1]);
-        int yy = Integer.parseInt(sb[2]);
+        int mm = Integer.parseInt(parts[0]);
+        int dd = Integer.parseInt(parts[1]);
+        int yy = Integer.parseInt(parts[2]);
         System.out.println(yy+ "." + mm + "." + dd);
     }
 }
