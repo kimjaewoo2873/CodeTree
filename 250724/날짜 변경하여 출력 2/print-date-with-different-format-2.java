b419a00c458758e419af69e1e368d034332c0951
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-24 00:13:50 +0900

    Title: 날짜 변경하여 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250724/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        StringBuffer sb = new StringBuffer(str, "-");
+        int mm = Integer.parseInt(sb[0]);
+        int dd = Integer.parseInt(sb[1]);
+        int yy = Integer.parseInt(sb[2]);
+        System.out.println(yy+ "." + mm + "." + dd);
+    }
+}
