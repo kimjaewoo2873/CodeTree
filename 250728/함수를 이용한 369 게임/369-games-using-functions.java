2784d40c1ced47fd0757b2fa167b44099206cb19
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-28 23:41:42 +0900

    Title: 함수를 이용한 369 게임, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250728/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static int findThirdMultiple(int a, int b) {
+        int cnt = 0;
+        int sec = 0;
+        int fir = 0;
+        for(int i=a; i<=b;i++) {
+            sec = i % 10;
+            fir = i / 10;
+            if(i % 3 == 0 || sec % 3 == 0 || fir % 3 ==0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        // Please write your code here.
+        System.out.println(findThirdMultiple(A, B));
+    }
+}
