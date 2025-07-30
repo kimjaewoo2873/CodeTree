c4dca67b759da6ce380f832fce2c92c0fe0af598
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-30 20:57:48 +0900

    Title: 함수를 이용한 온전수 판별, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250730/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void findCount(int a,int b) {
+        int cnt = 0;
+        for(int i=a;i<=b;i++) {
+            if(i % 2 == 0 || i % 10 == 5 || (i % 3 == 0 && i % 9 == 0))
+                continue;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Please write your code here.
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        findCount(a, b);
+    }
+}
