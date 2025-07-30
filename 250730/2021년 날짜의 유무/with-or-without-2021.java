0188352cd53bf236c63c66c0b63522704495fa5c
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-07-30 23:26:50 +0900

    Title: 2021년 날짜의 유무, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250730/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static String findDay(int m, int d) {
+        int year[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if(m >= 1 && m <= 12) {
+            if(year[m + 1] >= d) { 
+                return "Yes";
+            } else {
+                return "No";
+            }
+        }
+        return "No";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        // Please write your code here.
+        System.out.println(findDay(m, d));
+    }
+}
