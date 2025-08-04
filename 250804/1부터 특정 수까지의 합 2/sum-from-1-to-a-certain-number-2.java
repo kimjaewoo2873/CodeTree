22cf04017d73a794b175614eb5b8a020a62c9c58
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-08-04 23:35:08 +0900

    Title: 1부터 특정 수까지의 합 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250804/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
+++ 250804/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int sum = 0;
     public static void f(int n) {
         if(n == 0) return;
-        f(n - 1);
-        sum += n;
+        return f(n - 1) + n;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
-        f(n);
-        System.out.println(sum);
+        System.out.println(f(n));
     }
 }
