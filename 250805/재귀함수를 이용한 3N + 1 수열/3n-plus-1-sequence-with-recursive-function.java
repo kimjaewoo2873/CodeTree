a50c376d621694ea307515747374a0e2e1669be5
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-08-05 23:38:17 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250805/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static int cnt = 0;
+    public static int fun(int n) {
+        if(n == 1) return 0;
+
+        if(n % 2 == 0) {
+            return fun(n/2) + 1;
+        } else {
+            return fun(3*n+1)+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(fun(n));
+    }
+}
