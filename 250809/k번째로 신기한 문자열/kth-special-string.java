8fdd65ab205c9d069255140fe7cee9229a0124b5
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-08-09 01:06:39 +0900

    Title: k번째로 신기한 문자열, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250809/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void fun(int n, int k, String t, String []words) {
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> rest = new ArrayList<>();
+        for(int i=0;i<words.length;i++) {
+            if(words[i].indexOf(t) != -1) {
+                result.add(words[i]);
+            } else {
+                rest.add(words[i]);
+            }
+        }
+        
+        Collections.sort(result);
+        Collections.sort(rest);
+
+        result.addAll(rest);
+
+        System.out.println(result.get(k - 1));
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String t = sc.next();
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = sc.next();
+        }
+        // Please write your code here.
+        fun(n, k, t, words);
+    }
+}
