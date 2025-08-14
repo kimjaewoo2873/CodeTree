245fe51bf5a7cf83a14a560acc5e2c290eae8150
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-08-14 23:39:40 +0900

    Title: Date to Date, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 250814/Date to Date/date-to-date.java
+++ 250814/Date to Date/date-to-date.java
@@ -19,17 +19,17 @@ public class Main {
         int month[] = {31, 28,31,30,31,30,31,31,30,31, 30, 31};
         int v1 = 0;
         int v2 = 0;
-        for(int i=0;i<a.getMonth();i++){
+        for(int i=0;i<a.getMonth() - 1;i++){
             v1 += month[i];
         } 
         v1 += a.getDay();
         
-        for(int i=0;i<b.getMonth();i++){
+        for(int i=0;i<b.getMonth() - 1;i++){
             v2 += month[i];
         }
         v2 += b.getDay();
 
-        return (v2 - v1);
+        return (v2 - v1) + 1;
         
     }
     public static void main(String[] args) {
