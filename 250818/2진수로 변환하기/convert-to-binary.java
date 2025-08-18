f862abfe38a31fdc96058141c6b1aa5f882dacea
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-08-18 19:23:53 +0900

    Title: 2진수로 변환하기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250818/2진수로 변환하기/convert-to-binary.java
+++ 250818/2진수로 변환하기/convert-to-binary.java
@@ -5,15 +5,9 @@ public class Main {
     public static void findBinary(int n) {
         ArrayList<Integer> list = new ArrayList<>();
     
-        while (true) { 
-            if(n <= 2) {
-                list.add(n % 2);
-                break;
-            }
-            else {
-                list.add(n % 2);
-                n /= 2;
-            }
+        while (n > 0) { 
+            list.add(n % 2);
+            n /= 2;
         }
         
         for(int i = list.size() - 1; i >= 0; i--) {
