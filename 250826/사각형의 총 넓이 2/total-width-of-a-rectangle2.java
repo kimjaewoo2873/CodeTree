2041a3dbcfa1a9b5de16cfe74964fab1dcb4f7b8
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-08-26 20:30:08 +0900

    Title: 사각형의 총 넓이 2, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250826/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250826/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
 public class Main {
     public static void findArea(int n, int x1[], int y1[], int x2[], int y2[]) {
-        int sum = 0;
+        int a[][] = new int[201][201];
+        int cnt = 0;
         for(int i=0;i<n;i++) {
             int w1 = x1[i] + 100;
             int h1 = y1[i] + 100;
             int w2 = x2[i] + 100;
             int h2 = y2[i] + 100;
-            
-            int a = Math.abs((w2 - w1) * (h2 - h1));
-
-            sum += a;
-        }         
-        System.out.println(sum);
+            for(int k=w1;k<w2;k++) {
+                for(int j=h1;j<h2;j++) {
+                    if(a[k][j] != 1) {
+                        a[k][j] = 1;
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
