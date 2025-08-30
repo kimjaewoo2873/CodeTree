6e5d3190e91c5e8150722a1221c2e2bcb14d6328
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-08-30 17:32:10 +0900

    Title: 계속 중첩되는 사각형, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250830/계속 중첩되는 사각형/continuously-overlapping-squares.java
+++ 250830/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -1,40 +1,36 @@
 import java.util.Scanner;
-
 public class Main {
     static final int MAX = 201;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] area = new int[MAX][MAX];
+        int area[][] = new int[MAX][MAX];
         int cnt = 0;
-
         for (int i = 0; i < n; i++) {
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
             int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
-
+            int y2 = sc.nextInt(); 
             x1 += 100;
             y1 += 100;
             x2 += 100;
             y2 += 100;
-
-            for (int k = x1; k < x2; k++) {
-                for (int j = y1; j < y2; j++) {
-                    area[k][j] = 1;  // 그냥 덮기만 하면 됨
+            
+            int color = (i % 2 == 0) ? 1 : 2;
+            for(int k=x1; k<x2; k++) {
+                for(int j=y1; j<y2; j++) {
+                    area[k][j] = color;
                 }
             }
         }
-
-        for (int i = 0; i < MAX; i++) {
-            for (int j = 0; j < MAX; j++) {
-                if (area[i][j] == 1) {
+        for(int i=0;i<area.length;i++) {
+            for(int j=0;j<area[i].length;j++) {
+                if(area[i][j] == 2) {
                     cnt++;
                 }
             }
         }
-
         System.out.println(cnt);
     }
 }
