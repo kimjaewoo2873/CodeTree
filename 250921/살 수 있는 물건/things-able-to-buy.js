cfd401703b77cdfecadc8530bb651d03bcc3e1cb
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-09-21 23:20:28 +0900

    Title: 살 수 있는 물건, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250921/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if(a >= 3000) { 
+    console.log("book");
+} else if(a < 3000 && a >= 1000) {
+    console.log("mask");
+} else {
+    console.log("no");
+}
