f088b8ea20fbe6efda525d48292f755b90a633e1
Jaewoo Kim <72966886+kimjaewoo2873@users.noreply.github.com>
2025-09-21 22:24:50 +0900

    Title: 합과 차의 나눗셈, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250921/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let sum = a + b;
+let min = a - b;
+
+console.log((sum/min).toFixed(2));
